5cd7183d2b167f04af0f17d7d3505efc376f8fd2
Koen <me@cld.moe>
2020-10-22 20:30:40 +0200

    Add Builds

---
+++ vk/vulkan_test.go
@@ -0,0 +1,47 @@
+// +build test
+package vk
+
+import (
+	"bytes"
+	"testing"
+)
+
+func VK_MAKE_VERSION(major, minor, patch int) uint32 {
+    return ((((uint32)(major)) << 22) | (((uint32)(minor)) << 12) | ((uint32)(patch)))
+}
+
+func CString(b []byte) []byte {
+	return b[:bytes.IndexByte(b, 0)]
+}
+
+func TestHelloWorld(tst *testing.T) {
+	appInfo := VkApplicationInfo{};
+	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	appInfo.pApplicationName = &([]byte("Hello Triangle")[0]);
+	appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+	appInfo.pEngineName = &([]byte("None")[0])
+	appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+	appInfo.apiVersion = VK_MAKE_VERSION(1, 0, 0);
+
+	createInfo := VkInstanceCreateInfo{};
+	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	createInfo.pApplicationInfo = &appInfo;
+	createInfo.enabledLayerCount = 0;
+
+	var extensionCount uint32 = 0;
+	vkEnumerateInstanceExtensionProperties(nil, &extensionCount, nil);
+	tst.Logf("Num extensions: %v", extensionCount)
+	ext := make([]VkExtensionProperties,extensionCount)
+	vkEnumerateInstanceExtensionProperties(nil, &extensionCount, &ext[0]);
+	
+	tst.Logf("Ext[0] name: %s", CString(ext[0].extensionName[:]))
+	
+
+	var instance VkInstance
+	if e := vkCreateInstance(&createInfo, nil, &instance);e != VK_SUCCESS {
+		tst.Error(e)
+	}
+	tst.Logf("%v", instance)
+	vkDestroyInstance(instance, nil);
+	tst.Logf("%v", instance)
+}
